deed961ff68bcacc7fad78d7702bb21422c3adda
sidprogram <siddheshbhandge9691@gmail.com>
2022-11-04 01:20:47 +0530

    code added

--- test.java
+++ test.java
@@ -1,7 +1,16 @@
 public class Test{
+<<<<<<< Updated upstream
 
     public void add(){
         System.out.println("Add Method")
     }
     
+=======
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int c=a+b;
+    }
+    System.out.println("Addition is:" +c)
+>>>>>>> Stashed changes
 }
